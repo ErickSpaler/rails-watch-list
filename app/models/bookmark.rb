a2f3638333fad39d9f97996f7e23a1b9c2a3ff3f
erick spaler <erickspaler@hotmail.com>
2023-07-30 19:30:40 -0300

    validations done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,7 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
+  validates :movie_id, uniqueness: {scope: :list_id, message: 'is already in the list'}
+  validation :name, presence: true, uniqueness: true
 
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmark, dependent: :destroy
-  has_many :movies, dependent: :destroy
+  has_many :movies, through: :bookmarks, dependent: :destroy
 
   validation :name, presence: true, uniqueness: true
 end
