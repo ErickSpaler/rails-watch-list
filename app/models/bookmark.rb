8ee5128612a6d8b133d65d639a64d1643821e634
erick spaler <erickspaler@hotmail.com>
2023-07-31 14:10:07 -0300

    routes and index done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,7 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :movie_id, uniqueness: {scope: :list_id, message: 'is already in the list'}
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'is already in the list' }
   validation :name, presence: true, uniqueness: true
-
+  validates :comment, length: { minimum: 6 }
 end
